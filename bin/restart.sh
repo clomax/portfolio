ec4fb62e066f0fb63c84b84a8dbcf3c9b5e885d2
Craig Lomax <craig@clomax.me.uk>
2016-01-15 17:33:53 +0000

    Use correct path.

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Restart Unicorn
 
-project_home="/home/craig/www"
+project_home="/var/www/clomax.me.uk"
 unicorn_pid=$(cat $project_home/var/run/unicorn.pid)
 echo "Restarting Unicorn ($unicorn_pid)"
 kill -HUP $unicorn_pid
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 4
+worker_processes 1
 @app_path = '/var/www/clomax.me.uk/'
 
 listen "#{@app_path}var/run/unicorn.sock", backlog: 64
